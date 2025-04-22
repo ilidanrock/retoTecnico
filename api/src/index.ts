a606b3499488237500f46de6ab3bc73d24d1cdff
Luis Rivera <rluis747@gmail.com>
2025-04-22 10:37:59 -0500

    reto tecnico

---
+++ api/src/index.ts
@@ -0,0 +1,39 @@
+// src/index.ts
+import express from 'express';
+import { RandomUserResponse, User } from './types';
+
+
+
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/api/users', async (_req, res) => {
+  try {
+    const resp = await fetch('https://randomuser.me/api/?results=10');
+    
+
+    const data = (await resp.json()) as RandomUserResponse;
+    
+
+    const { results } = data;
+
+    const users = results.map((u: User) => ({
+      nombre:            `${u.name.first} ${u.name.last}`,
+      genero:            u.gender,
+      ubicacion:         `${u.location.city}, ${u.location.country}`,
+      correo:            u.email,
+      fechaNacimiento:   u.dob.date,
+      fotografia:        u.picture.large,
+    }));
+
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al obtener usuarios' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server listening on http://localhost:${PORT}/api/users`);
+});
